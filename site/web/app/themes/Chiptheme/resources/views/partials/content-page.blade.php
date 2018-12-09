6c47e0bf49949f9f17f0e877534b2052588e4fc8
Thomas <nukeclears@gmail.com>
2018-12-09 21:56:31 +0100

    main content mostly done

--- site/web/app/themes/Chiptheme/resources/views/partials/content-page.blade.php
+++ site/web/app/themes/Chiptheme/resources/views/partials/content-page.blade.php
@@ -1,10 +1,18 @@
 <div class="main_content">
-    <div class="content_container d-flex flex-row">
+    <div class="content_container d-flex">
         <div class="left_block">
-            
+            <h1>Nut of the day</h1>
+            <h2>WALNUT!</h2>
+            <p>Walnuts are rounded, single-seeded stone fruits of the walnut tree commonly
+             used for the meat after fully ripening. 
+             Following full ripening, the removal of the husk reveals the wrinkly walnut shell, 
+             which is usually commercially found in two segments (three-segment 
+             shells can also form). During the ripening process, 
+             the husk will become brittle and the shell hard.</p>
+            <button>Find more nuts</button>
         </div>
         <div class="right_block">
-
+            <img src="@asset('images/walnut.png')"/>
         </div>
     </div>
 </div>
